c505490d7229cde715cadceb01ee52a260ed8189
TruongSa <shrimp2t@gmail.com>
2016-02-12 00:30:17 +0700

    Update upsell panel

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -352,6 +352,7 @@ jQuery( window ).ready( function( $ ){
         }
         if ( onepress_customizer_settings.is_plus_activated !== 'y' ) {
             $('#customize-info .accordion-section-title').append('<a target="_blank" style="text-transform: uppercase; background: #D54E21; color: #fff; font-size: 10px; line-height: 14px; padding: 2px 5px; display: inline-block;" href="https://www.famethemes.com/themes/onepress/?utm_source=theme_customizer&utm_medium=text_link&utm_campaign=onepress_customizer#get-started">Upgrade to OnePress plus</a>');
+            $( '#accordion-section-onepress_order_styling > .accordion-section-title').append( '<span class="onepress-notice">Plus</span>' );
         }
     }
 
