0f562f69a68fada0a218f662ebb683c2f2602821
TruongSa <shrimp2t@gmail.com>
2016-02-23 13:15:10 +0700

    Add frontpage template hooks

--- template-frontpage.php
+++ template-frontpage.php
@@ -11,11 +11,13 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
             <?php
 
+            do_action( 'onepress_frontpage_before_section_parts' );
+
 			if ( ! has_action( 'onepress_frontpage_section_parts' ) ) {
 
-				$sections = array(
-					'hero', 'about', 'services', 'videolightbox', 'counter', 'team', 'news', 'contact'
-				);
+				$sections = apply_filters( 'onepress_frontpage_sections_order', array(
+                    'hero', 'about', 'services', 'videolightbox', 'counter', 'team', 'news', 'contact'
+                ) );
 
 				foreach ( $sections as $section ){
 					get_template_part('section-parts/section', $section );
@@ -24,6 +26,8 @@ get_header(); ?>
 			} else {
 				do_action( 'onepress_frontpage_section_parts' );
 			}
+
+            do_action( 'onepress_frontpage_after_section_parts' );
 
 			?>
 		</main><!-- #main -->
