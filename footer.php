95b9fedde42a0622094a896e8843252d4b7fb9b1
shrimp2t <shrimp2t@gmail.com>
2016-03-07 23:47:59 +0700

    Subscribe button translateable.

--- footer.php
+++ footer.php
@@ -35,7 +35,7 @@ $onepress_newsletter_mailchimp = get_theme_mod( 'onepress_newsletter_mailchimp' 
 							<?php if ( $onepress_newsletter_title != '' ) echo '<h5 class="follow-heading">'. $onepress_newsletter_title .'</h5>'; ?>
 							<form novalidate="" target="_blank" class="" name="mc-embedded-subscribe-form" id="mc-embedded-subscribe-form" method="post" action="<?php if ( $onepress_newsletter_mailchimp != '' ) echo $onepress_newsletter_mailchimp; ?>">
 								<input type="text" placeholder="<?php esc_attr_e( 'Enter your e-mail address', 'onepress' ); ?>" id="mce-EMAIL" class="subs_input" name="EMAIL" value="">
-								<input type="submit" class="subs-button" value="Subscribe" name="subscribe">
+								<input type="submit" class="subs-button" value="<?php esc_attr_e( 'Subscribe', 'onepress' ); ?>" name="subscribe">
 							 </form>
 						</div>
 					</div>
