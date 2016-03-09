0fed6068a9900b7798e69fe47a63dcb8f77baec2
Kien T <kienpc81089@gmail.com>
2016-03-09 15:47:42 +0700

    Section Features - HTML

---
+++ section-parts/section-features.php
@@ -0,0 +1,53 @@
+<section id="" class="section-features section-padding section-meta onepage-section">
+    <div class="container">
+        <div class="section-title-area">
+            <h5 class="section-subtitle">Why choose Us</h5>
+            <h2 class="section-title">Features</h2>
+            <div class="section-desc">This is section description, just like about section.</div>
+        </div>
+        <div class="section-content">
+            <div class="row">
+                <div class="feature-item col-lg-3 col-md-6 wow slideInUp">
+                    <div class="feature-media">
+                        <span class="fa-stack fa-5x">
+                            <i class="fa fa-circle fa-stack-2x icon-background-default"></i>
+                            <i class="feature-icon fa fa-laptop fa-stack-1x"></i>
+                        </span>
+                    </div>
+                    <h4>Responsive Design</h4>
+                    <p>The theme is responsive, your website will look great on touch devices.</p>
+                </div>
+                <div class="feature-item col-lg-3 col-md-6 wow slideInUp">
+                    <div class="feature-media">
+                        <span class="fa-stack fa-5x">
+                            <i class="fa fa-circle fa-stack-2x icon-background-default"></i>
+                            <i class="feature-icon fa fa-photo fa-stack-1x"></i>
+                        </span>
+                    </div>
+                    <h4>Parallax Effect</h4>
+                    <p>Create memorable pages with smooth parallax effects that everyone loves.</p>
+                </div>
+                <div class="feature-item col-lg-3 col-md-6 wow slideInUp">
+                    <div class="feature-media">
+                        <span class="fa-stack fa-5x">
+                            <i class="fa fa-circle fa-stack-2x icon-background-default"></i>
+                            <i class="feature-icon fa fa-cart-arrow-down fa-stack-1x"></i>
+                        </span>
+                    </div>
+                    <h4>WooCommerce</h4>
+                    <p>Build a front page for your WooCommerce store in minutes.</p>
+                </div>
+                <div class="feature-item col-lg-3 col-md-6 wow slideInUp">
+                    <div class="feature-media">
+                        <span class="fa-stack fa-5x">
+                            <i class="fa fa-circle fa-stack-2x icon-background-default"></i>
+                            <i class="feature-icon fa fa-reorder fa-stack-1x"></i>
+                        </span>
+                    </div>
+                    <h4>Content blocks</h4>
+                    <p>Change the sections contents, display each block exactly where you need it.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
