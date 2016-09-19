a17d2b7d624e87c82731b9aabf976af779d493c8
TruongSa <shrimp2t@gmail.com>
2016-09-20 00:15:17 +0700

    Improve perfomance

--- inc/customizer-controls.php
+++ inc/customizer-controls.php
@@ -206,10 +206,9 @@ function onepress_sanitize_repeatable_data_field( $input , $setting ){
             }
 
         }
-
     }
 
-    return json_encode( $data );
+    return $data;
 }
 
 
@@ -231,41 +230,6 @@ class OnePress_Editor_Custom_Control extends WP_Customize_Control
      */
     public $mod;
 
-    /**
-     * Enqueue scripts/styles.
-     *
-     * @since  1.0.0
-     * @access public
-     * @return void
-     */
-    public function enqueue() {
-        wp_enqueue_script( 'wp-color-picker' );
-        wp_register_script( 'onepress-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-controls', 'wp-color-picker' ) );
-        wp_register_style( 'onepress-customizer',  get_template_directory_uri() . '/assets/css/customizer.css' );
-
-        wp_enqueue_script( 'onepress-customizer' );
-        wp_enqueue_style( 'onepress-customizer' );
-
-
-        if ( ! class_exists( '_WP_Editors' ) ) {
-            require(ABSPATH . WPINC . '/class-wp-editor.php');
-        }
-
-        add_action( 'customize_controls_print_footer_scripts', array( __CLASS__, 'enqueue_editor' ),  2 );
-        add_action( 'customize_controls_print_footer_scripts', array( '_WP_Editors', 'editor_js' ), 50 );
-        add_action( 'customize_controls_print_footer_scripts', array( '_WP_Editors', 'enqueue_scripts' ), 1 );
-    }
-
-    public  static function enqueue_editor(){
-        if( ! isset( $GLOBALS['__wp_mce_editor__'] ) || ! $GLOBALS['__wp_mce_editor__'] ) {
-            $GLOBALS['__wp_mce_editor__'] = true;
-            ?>
-            <script id="_wp-mce-editor-tpl" type="text/html">
-                <?php wp_editor('', '__wp_mce_editor__'); ?>
-            </script>
-            <?php
-        }
-    }
     public function render_content() {
         $this->mod = strtolower( $this->mod );
         if( ! $this->mod = 'html' ) {
@@ -330,12 +294,7 @@ class OnePress_Alpha_Color_Control extends WP_Customize_Control {
      * stand alone class we'll register and enqueue them here.
      */
     public function enqueue() {
-        wp_enqueue_script( 'wp-color-picker' );
-        wp_register_script( 'onepress-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-controls', 'wp-color-picker' ) );
-        wp_register_style( 'onepress-customizer',  get_template_directory_uri() . '/assets/css/customizer.css' );
 
-        wp_enqueue_script( 'onepress-customizer' );
-        wp_enqueue_style( 'onepress-customizer' );
     }
 
     /**
@@ -488,12 +447,14 @@ class Onepress_Customize_Repeatable_Control extends WP_Customize_Control {
     public function to_json() {
         parent::to_json();
         $value = $this->value();
-        if ( empty ( $value ) ){
-            $value = json_encode( $this->defined_values );
-        } elseif ( is_array( $this->defined_values ) && ! empty ( $this->defined_values ) ) {
+
+        if (is_string( $value ) ) {
             $value = json_decode( $value, true );
+        }
+        if ( empty ( $value ) ){
+            $value = $this->defined_values;
+        } elseif ( is_array( $this->defined_values ) && ! empty ( $this->defined_values ) ) {
             $value = $this->merge_data( $value, $this->defined_values );
-            $value = json_encode( $value );
         }
 
         $this->json['live_title_id'] = $this->live_title_id;
@@ -516,42 +477,18 @@ class Onepress_Customize_Repeatable_Control extends WP_Customize_Control {
      * @return void
      */
     public function enqueue() {
-
-        wp_enqueue_media();
-
-        wp_enqueue_script( 'jquery-ui-sortable' );
-        wp_enqueue_script( 'wp-color-picker' );
-        wp_enqueue_style( 'wp-color-picker' );
-
-        wp_register_script( 'onepress-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-controls', 'wp-color-picker' ) );
-        wp_register_style( 'onepress-customizer',  get_template_directory_uri() . '/assets/css/customizer.css' );
-
-        wp_enqueue_script( 'onepress-customizer' );
-        wp_enqueue_style( 'onepress-customizer' );
-
-        if ( ! class_exists( '_WP_Editors' ) ) {
-            require(ABSPATH . WPINC . '/class-wp-editor.php');
-        }
-
-        add_action( 'customize_controls_print_footer_scripts', array( __CLASS__, 'enqueue_editor' ),  2 );
-        add_action( 'customize_controls_print_footer_scripts', array( '_WP_Editors', 'editor_js' ), 50 );
-        add_action( 'customize_controls_print_footer_scripts', array( '_WP_Editors', 'enqueue_scripts' ), 1 );
-
+        add_action( 'customize_controls_print_footer_scripts', array( __CLASS__, 'item_tpl' ), 66 );
     }
 
-    public  static function enqueue_editor(){
-        if( ! isset( $GLOBALS['__wp_mce_editor__'] ) || ! $GLOBALS['__wp_mce_editor__'] ) {
-            $GLOBALS['__wp_mce_editor__'] = true;
-            ?>
-            <script id="_wp-mce-editor-tpl" type="text/html">
-                <?php wp_editor('', '__wp_mce_editor__'); ?>
-            </script>
+    public static function item_tpl(){
+        ?>
+        <script type="text/html" id="repeatable-js-item-tpl">
+            <?php self::js_item(); ?>
+        </script>
         <?php
-        }
     }
 
     public function render_content() {
-
         ?>
         <label>
             <?php if ( ! empty( $this->label ) ) : ?>
@@ -561,25 +498,17 @@ class Onepress_Customize_Repeatable_Control extends WP_Customize_Control {
                 <span class="description customize-control-description"><?php echo $this->description; ?></span>
             <?php endif; ?>
         </label>
-
         <input data-hidden-value type="hidden" <?php $this->input_attrs(); ?> value="" <?php $this->link(); ?> />
-
         <div class="form-data">
-            <ul class="list-repeatable">
-            </ul>
+            <ul class="list-repeatable"></ul>
         </div>
-
         <div class="repeatable-actions">
             <span class="button-secondary add-new-repeat-item"><?php _e( 'Add an item', 'onepress' ); ?></span>
         </div>
-
-         <script type="text/html" class="repeatable-js-template">
-            <?php $this->js_item(); ?>
-        </script>
         <?php
     }
 
-    public function js_item( ){
+    public static function js_item(){
 
         ?>
         <li class="repeatable-customize-control">
@@ -601,8 +530,6 @@ class Onepress_Customize_Repeatable_Control extends WP_Customize_Control {
                                 <# if ( ! data.hasOwnProperty( i ) ) continue; #>
                                 <# field = data[i]; #>
                                 <# if ( ! field.type ) continue; #>
-
-
                                 <# if ( field.type ){ #>
 
                                     <#
@@ -625,7 +552,6 @@ class Onepress_Customize_Repeatable_Control extends WP_Customize_Control {
                                             <p class="field-desc description">{{{ field.desc }}}</p>
                                             <# } #>
                                         <# } #>
-
 
                                         <# if ( field.type === 'hidden' ) { #>
                                             <input data-live-id="{{ field.id }}" type="hidden" value="{{ field.value }}" data-repeat-name="_items[__i__][{{ field.id }}]" class="">
@@ -655,13 +581,11 @@ class Onepress_Customize_Repeatable_Control extends WP_Customize_Control {
                                             <# } #>
 
                                                 <# for ( k in field.options ) { #>
-
                                                     <# if ( _.isArray( field.value ) ) { #>
                                                         <option <# if ( _.contains( field.value , k ) ) { #> selected="selected" <# } #>  value="{{ k }}">{{ field.options[k] }}</option>
                                                     <# } else { #>
                                                         <option <# if ( field.value == k ) { #> selected="selected" <# } #>  value="{{ k }}">{{ field.options[k] }}</option>
                                                     <# } #>
-
                                                 <# } #>
 
                                             </select>
@@ -716,13 +640,10 @@ class Onepress_Customize_Repeatable_Control extends WP_Customize_Control {
                                             </div>
 
                                         <# } else if ( field.type == 'textarea' || field.type == 'editor' ) { #>
-
                                             <textarea data-live-id="{{{ field.id }}}" data-repeat-name="_items[__i__][{{ field.id }}]">{{ field.value }}</textarea>
-
                                         <# } #>
 
                                     </div>
-
 
                                 <# } #>
                             <# } #>
@@ -750,3 +671,64 @@ class Onepress_Customize_Repeatable_Control extends WP_Customize_Control {
     }
 
 }
+
+
+function onepress_enqueue_editor(){
+    if( ! isset( $GLOBALS['__wp_mce_editor__'] ) || ! $GLOBALS['__wp_mce_editor__'] ) {
+        $GLOBALS['__wp_mce_editor__'] = true;
+        ?>
+        <script id="_wp-mce-editor-tpl" type="text/html">
+            <?php wp_editor('', '__wp_mce_editor__'); ?>
+        </script>
+        <?php
+    }
+}
+
+
+class OnePress_Editor_Scripts
+{
+    /**
+     * Enqueue scripts/styles.
+     *
+     * @since  1.0.0
+     * @access public
+     * @return void
+     */
+    public static function enqueue() {
+
+        if ( ! class_exists( '_WP_Editors' ) ) {
+            require(ABSPATH . WPINC . '/class-wp-editor.php');
+        }
+
+        add_action( 'customize_controls_print_footer_scripts', array( __CLASS__, 'enqueue_editor' ),  2 );
+        add_action( 'customize_controls_print_footer_scripts', array( '_WP_Editors', 'editor_js' ), 50 );
+        add_action( 'customize_controls_print_footer_scripts', array( '_WP_Editors', 'enqueue_scripts' ), 1 );
+    }
+
+    public  static function enqueue_editor(){
+        if( ! isset( $GLOBALS['__wp_mce_editor__'] ) || ! $GLOBALS['__wp_mce_editor__'] ) {
+            $GLOBALS['__wp_mce_editor__'] = true;
+            ?>
+            <script id="_wp-mce-editor-tpl" type="text/html">
+                <?php wp_editor('', '__wp_mce_editor__'); ?>
+            </script>
+            <?php
+        }
+    }
+}
+
+
+function onepres_customizer_control_scripts(){
+    wp_enqueue_media();
+    wp_enqueue_script( 'jquery-ui-sortable' );
+    wp_enqueue_script( 'wp-color-picker' );
+    wp_enqueue_style( 'wp-color-picker' );
+
+    wp_enqueue_script( 'onepress-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-controls', 'wp-color-picker' ) );
+    wp_enqueue_style( 'onepress-customizer',  get_template_directory_uri() . '/assets/css/customizer.css' );
+
+}
+
+add_action( 'customize_controls_enqueue_scripts', 'onepres_customizer_control_scripts', 99 );
+add_action( 'customize_controls_enqueue_scripts', array( 'OnePress_Editor_Scripts', 'enqueue' ), 95 );
+
