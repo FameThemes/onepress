68aa1e1af9077d3b14e2ffc081fb0d804dd89fcb
TruongSa <shrimp2t@gmail.com>
2016-11-01 16:14:46 +0700

    Add sidebar for Woo fixed #149

--- functions.php
+++ functions.php
@@ -125,6 +125,18 @@ function onepress_widgets_init() {
 		'after_title'   => '</h2>',
 	) );
 
+    if ( class_exists( 'WooCommerce' ) ) {
+        register_sidebar( array(
+            'name'          => esc_html__( 'Shop Sidebar', 'onepress' ),
+            'id'            => 'sidebar-shop',
+            'description'   => '',
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</aside>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        ) );
+    }
+
 }
 add_action( 'widgets_init', 'onepress_widgets_init' );
 
