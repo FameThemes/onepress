b4a9c7b475f88b7efbf5b960bfa875cd64e48864
TruongSa <shrimp2t@gmail.com>
2017-03-08 09:34:15 +0700

    New parallax

--- section-parts/section-videolightbox.php
+++ section-parts/section-videolightbox.php
@@ -11,33 +11,30 @@ if ( ( ! $disable && ( $video || $heading ) )  || onepress_is_selective_refresh(
     $image    = get_theme_mod( 'onepress_videolightbox_image' );
     if ( ! onepress_is_selective_refresh() ){
     ?>
-    <?php if ( $image ) { ?>
-    <div id="parallax-<?php echo esc_attr( $id ); ?>" class="parallax-<?php echo esc_attr( $id ); ?> parallax-window" data-over-scroll-fix="false" data-z-index="1" data-speed="0.3" data-image-src="<?php echo esc_url( $image ); ?>" data-parallax="scroll" data-position="center" data-bleed="0">
-    <?php } ?>
-        <section id="<?php if ($id != '') echo esc_attr( $id ); ?>" <?php do_action('onepress_section_atts', 'videolightbox'); ?>
-             class="<?php echo esc_attr(apply_filters('onepress_section_class', 'section-videolightbox section-padding section-padding-larger section-inverse onepage-section', 'videolightbox')); ?>">
+    <section id="<?php if ($id != '') echo esc_attr( $id ); ?>" <?php do_action('onepress_section_atts', 'videolightbox'); ?>
+         class="<?php echo esc_attr(apply_filters('onepress_section_class', 'section-videolightbox section-padding section-padding-larger section-inverse onepage-section section-parallax', 'videolightbox')); ?>">
+        <div class="parallax-bg" data-stellar-ratio="0.5" data-stellar-offset-parent="true"  data-stellar-ratio="0.5" data-stellar-offset-parent="true" style="background-image: url('<?php echo esc_url( $image ); ?>');"></div>
     <?php } ?>
 
-            <?php do_action('onepress_section_before_inner', 'videolightbox'); ?>
-            <div class="container">
-                <?php if ( $video ) { ?>
-                <div class="videolightbox__icon videolightbox-popup">
-                    <a href="<?php echo esc_attr( $video ); ?>" data-scr="<?php echo esc_attr( $video ); ?>" class="popup-video">
-                        <span class="video_icon"><i class="fa fa-play"></i></span>
-                    </a>
-                </div>
-                <?php } ?>
-                <?php if ( $heading ) { ?>
-                <h2 class="videolightbox__heading"><?php echo do_shortcode( wp_kses_post( $heading ) ); ?></h2>
-                <?php } ?>
+        <?php do_action('onepress_section_before_inner', 'videolightbox'); ?>
+        <div class="container">
+            <?php if ( $video ) { ?>
+            <div class="videolightbox__icon videolightbox-popup">
+                <a href="<?php echo esc_attr( $video ); ?>" data-scr="<?php echo esc_attr( $video ); ?>" class="popup-video">
+                    <span class="video_icon"><i class="fa fa-play"></i></span>
+                </a>
             </div>
-            <?php do_action('onepress_section_after_inner', 'videolightbox'); ?>
-
-    <?php if ( ! onepress_is_selective_refresh() ) { ?>
-        </section>
-        <?php if ($image) { ?>
+            <?php } ?>
+            <?php if ( $heading ) { ?>
+            <h2 class="videolightbox__heading"><?php echo do_shortcode( wp_kses_post( $heading ) ); ?></h2>
+            <?php } ?>
         </div>
-        <?php }
+        <?php do_action('onepress_section_after_inner', 'videolightbox'); ?>
+
+        <?php if ( ! onepress_is_selective_refresh() ) { ?>
+
+    </section>
+        <?php
     }?>
 
 <?php
