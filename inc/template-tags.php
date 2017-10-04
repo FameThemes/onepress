9382f95f769b4fa3aa45f1ea7e5ab2a12bddb05a
congthien <luckystar.ictu90@gmail.com>
2017-10-04 10:15:57 +0700

    add rel attribute for author tag.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -110,7 +110,7 @@ if ( ! function_exists( 'onepress_posted_on' ) ) {
 
         $byline = sprintf(
             esc_html_x('by %s', 'post author', 'onepress'),
-            '<span class="author vcard"><a class="url fn n" href="' . esc_url(get_author_posts_url(get_the_author_meta('ID'))) . '">' . esc_html(get_the_author()) . '</a></span>'
+            '<span class="author vcard"><a  rel="author" class="url fn n" href="' . esc_url(get_author_posts_url(get_the_author_meta('ID'))) . '">' . esc_html(get_the_author()) . '</a></span>'
         );
 
         echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
