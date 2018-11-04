58fcc5e9080c844336c33f3083cb8801a3208baf
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2018-11-04 23:26:35 +0100

    Fix formatting
    * Indentation https://make.wordpress.org/core/handbook/best-practices/coding-standards/php/#indentation
    * Space Usage https://make.wordpress.org/core/handbook/best-practices/coding-standards/php/#space-usage

--- woocommerce.php
+++ woocommerce.php
@@ -8,8 +8,8 @@
 get_header();
 
 $is_active_sidebar = is_active_sidebar( 'sidebar-shop' );
-$layout = onepress_get_layout();
-$hide_breadcrumb = get_post_meta( wc_get_page_id('shop'), '_hide_breadcrumb', true);
+$layout            = onepress_get_layout();
+$hide_breadcrumb   = get_post_meta( wc_get_page_id( 'shop' ), '_hide_breadcrumb', true );
 /**
  * @since 2.0.0
  * @see onepress_display_page_title
@@ -17,30 +17,29 @@ $hide_breadcrumb = get_post_meta( wc_get_page_id('shop'), '_hide_breadcrumb', tr
 do_action( 'onepress_page_before_content' );
 ?>
 
-	<div id="content" class="site-content">
-		<?php
-        if ( !  $hide_breadcrumb ) {
-            onepress_breadcrumb( wc_get_page_id('shop') );
-        }
-		?>
+<div id="content" class="site-content">
+	<?php
+	if ( ! $hide_breadcrumb ) {
+		onepress_breadcrumb( wc_get_page_id( 'shop' ) );
+	}
+	?>
 
-		<div id="content-inside" class="container <?php echo ( $is_active_sidebar ) ? esc_attr( $layout ) : 'no-sidebar'; ?>">
-			<div id="primary" class="content-area">
-				<main id="main" class="site-main" role="main">
-					<?php woocommerce_content(); ?>
-				</main><!-- #main -->
-			</div><!-- #primary -->
-            <?php if ( $is_active_sidebar ) { ?>
-                <?php if ( 'no-sidebar' != $layout ) { ?>
-                    <div id="secondary" class="widget-area sidebar" role="complementary">
-                        <?php dynamic_sidebar( 'sidebar-shop' ); ?>
-                    </div><!-- #secondary -->
-                <?php } ?>
+	<div id="content-inside" class="container <?php echo ( $is_active_sidebar ) ? esc_attr( $layout ) : 'no-sidebar'; ?>">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+				<?php woocommerce_content(); ?>
+			</main><!-- #main -->
+		</div><!-- #primary -->
+		<?php if ( $is_active_sidebar ) { ?>
+			<?php if ( 'no-sidebar' != $layout ) { ?>
+				<div id="secondary" class="widget-area sidebar" role="complementary">
+					<?php dynamic_sidebar( 'sidebar-shop' ); ?>
+				</div><!-- #secondary -->
+			<?php } ?>
 
-            <?php } ?>
-		</div><!--#content-inside -->
+		<?php } ?>
+	</div><!--#content-inside -->
 
-
-	</div><!-- #content -->
+</div><!-- #content -->
 
 <?php get_footer(); ?>
