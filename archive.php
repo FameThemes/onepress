381eae84c182e8d5916430183f1e3cd8287e6850
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2018-11-05 10:36:42 +0100

    Fix identation

--- archive.php
+++ archive.php
@@ -34,12 +34,12 @@ $layout = onepress_get_layout();
 
 					<?php
 
-						/*
-							* Include the Post-Format-specific template for the content.
-							* If you want to override this in a child theme, then include a file
-							* called content-___.php (where ___ is the Post Format name) and that will be used instead.
-							*/
-						get_template_part( 'template-parts/content', 'list' );
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', 'list' );
 					?>
 
 				<?php endwhile; ?>
