1b33a8e7aa20084c9ef6f40621c3f34457e8f0d5
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2018-11-05 10:28:34 +0100

    Fix indenation

--- index.php
+++ index.php
@@ -38,12 +38,12 @@ $layout = onepress_get_layout();
 
 					<?php
 
-						/*
-							* Include the Post-Format-specific template for the content.
-							* If you want to override this in a child theme, then include a file
-							* called content-___.php (where ___ is the Post Format name) and that will be used instead.
-							*/
-						get_template_part( 'template-parts/content', get_post_format() );
+					/*
+						* Include the Post-Format-specific template for the content.
+						* If you want to override this in a child theme, then include a file
+						* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						*/
+					get_template_part( 'template-parts/content', get_post_format() );
 					?>
 
 				<?php endwhile; ?>
