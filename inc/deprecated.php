9178f7b926dbed5200b57e259b1ed11b666c308b
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2018-11-11 01:43:01 +0100

    Remove onepress_enqueue_editor completly
    Not deprecated but is method of class  OnePress_Editor_Scripts

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -7,22 +7,6 @@
 
 /**
  * @deprecated 2.0.0
- * @link inc/customize-controls/control-pages.php
- */
-if ( ! function_exists( 'onepress_enqueue_editor' ) ) {
-	function onepress_enqueue_editor() {
-		if ( ! isset( $GLOBALS['__wp_mce_editor__'] ) || ! $GLOBALS['__wp_mce_editor__'] ) {
-			$GLOBALS['__wp_mce_editor__'] = true;
-
-			echo '<script id="_wp-mce-editor-tpl" type="text/html">';
-			wp_editor( '', '__wp_mce_editor__' );
-			echo '</script>';
-		}
-	}
-}
-
-/**
- * @deprecated 2.0.0
  * @link inc/sanitize.php
  */
 if ( ! function_exists( 'onepress_sanitize_hex_color' ) ) {
