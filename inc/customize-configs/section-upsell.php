3fc7d017729b60eaf2f28516bdf4987816085e03
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2018-11-11 02:41:58 +0100

    Add sanitization

--- inc/customize-configs/section-upsell.php
+++ inc/customize-configs/section-upsell.php
@@ -8,6 +8,7 @@
 
 // Register custom section types.
 $wp_customize->register_section_type( 'OnePress_Section_Plus' );
+
 // Register sections.
 $wp_customize->add_section(
 	'onepress_order_styling_preview',
@@ -17,7 +18,7 @@ $wp_customize->add_section(
 	)
 );
 
-// Plus message
+// Plus message.
 $wp_customize->add_setting(
 	'onepress_order_styling_message',
 	array(
@@ -30,18 +31,19 @@ $wp_customize->add_control(
 		array(
 			'type'        => 'custom_message',
 			'section'     => 'onepress_order_styling_preview',
-			'description' => __( '<h4 class="customizer-group-heading-message">Drag &amp; Drop Section Orders</h4><p class="customizer-group-heading-message">Check out the <a target="_blank" href="https://www.famethemes.com/plugins/onepress-plus/?utm_source=theme_customizer&utm_medium=text_link&utm_campaign=onepress_customizer#get-started">OnePress Plus</a> version for full control over the frontpage SECTIONS ORDER!</p><h4 class="customizer-group-heading-message">Advanced Section Styling</h4><p class="customizer-group-heading-message">Check out the <a target="_blank" href="https://www.famethemes.com/plugins/onepress-plus/?utm_source=theme_customizer&utm_medium=text_link&utm_campaign=onepress_customizer#get-started">OnePress Plus</a> version for full control over the section styling which includes background color, image, video, parallax effect, custom style and more ...</p>', 'onepress' ),
+			'description' => wp_kses_post( __( '<h4 class="customizer-group-heading-message">Drag &amp; Drop Section Orders</h4><p class="customizer-group-heading-message">Check out the <a target="_blank" href="https://www.famethemes.com/plugins/onepress-plus/?utm_source=theme_customizer&utm_medium=text_link&utm_campaign=onepress_customizer#get-started">OnePress Plus</a> version for full control over the frontpage SECTIONS ORDER!</p><h4 class="customizer-group-heading-message">Advanced Section Styling</h4><p class="customizer-group-heading-message">Check out the <a target="_blank" href="https://www.famethemes.com/plugins/onepress-plus/?utm_source=theme_customizer&utm_medium=text_link&utm_campaign=onepress_customizer#get-started">OnePress Plus</a> version for full control over the section styling which includes background color, image, video, parallax effect, custom style and more ...</p>', 'onepress' ) ),
 		)
 	)
 );
 
+// Add highlighted Upsell section above all other sections.
 $wp_customize->add_section(
 	new OnePress_Section_Plus(
 		$wp_customize,
 		'onepress-plus',
 		array(
 			'priority'  => 1,
-			'plus_text' => __( 'OnePress Plus Available! Take a look', 'onepress' ),
+			'plus_text' => esc_html__( 'OnePress Plus Available! Take a look', 'onepress' ),
 			'plus_url'  => 'https://www.famethemes.com/plugins/onepress-plus/?utm_source=theme_customizer&utm_medium=text_link&utm_campaign=onepress_customizer#get-started',
 		)
 	)
