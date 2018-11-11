0ff8cec9d68a7636699a6ade08381eb00866ef68
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2018-11-11 23:35:01 +0100

    Add onepress_get_animatecss_choices()

--- inc/customizer.php
+++ inc/customizer.php
@@ -6,6 +6,100 @@
  */
 
 /**
+ * Retrieve animate.css animations as choice array for select.
+ *
+ * @see https://github.com/daneden/animate.css
+ *
+ * @since x.x.x
+ * @return array
+ */
+function onepress_get_animatecss_choices() {
+	$animatecss = array(
+		'bounce',
+		'flash',
+		'pulse',
+		'rubberBand',
+		'shake',
+		'headShake',
+		'swing',
+		'tada',
+		'wobble',
+		'jello',
+		'bounceIn',
+		'bounceInDown',
+		'bounceInLeft',
+		'bounceInRight',
+		'bounceInUp',
+		'bounceOut',
+		'bounceOutDown',
+		'bounceOutLeft',
+		'bounceOutRight',
+		'bounceOutUp',
+		'fadeIn',
+		'fadeInDown',
+		'fadeInDownBig',
+		'fadeInLeft',
+		'fadeInLeftBig',
+		'fadeInRight',
+		'fadeInRightBig',
+		'fadeInUp',
+		'fadeInUpBig',
+		'fadeOut',
+		'fadeOutDown',
+		'fadeOutDownBig',
+		'fadeOutLeft',
+		'fadeOutLeftBig',
+		'fadeOutRight',
+		'fadeOutRightBig',
+		'fadeOutUp',
+		'fadeOutUpBig',
+		'flipInX',
+		'flipInY',
+		'flipOutX',
+		'flipOutY',
+		'lightSpeedIn',
+		'lightSpeedOut',
+		'rotateIn',
+		'rotateInDownLeft',
+		'rotateInDownRight',
+		'rotateInUpLeft',
+		'rotateInUpRight',
+		'rotateOut',
+		'rotateOutDownLeft',
+		'rotateOutDownRight',
+		'rotateOutUpLeft',
+		'rotateOutUpRight',
+		'hinge',
+		'rollIn',
+		'rollOut',
+		'zoomIn',
+		'zoomInDown',
+		'zoomInLeft',
+		'zoomInRight',
+		'zoomInUp',
+		'zoomOut',
+		'zoomOutDown',
+		'zoomOutLeft',
+		'zoomOutRight',
+		'zoomOutUp',
+		'slideInDown',
+		'slideInLeft',
+		'slideInRight',
+		'slideInUp',
+		'slideOutDown',
+		'slideOutLeft',
+		'slideOutRight',
+		'slideOutUp',
+	);
+
+	$choices = array();
+	foreach ( $animatecss as $v ) {
+		$choices[ $v ] = $v;
+	}
+	return $choices;
+}
+
+/**
  * Add horizontal line
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
