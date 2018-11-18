cee39bbe360b5615fec8d73366c0e9df8ef989c4
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2018-11-18 14:05:29 +0100

    Conditionally add pingback header

--- inc/extras.php
+++ inc/extras.php
@@ -8,6 +8,16 @@
  */
 
 /**
+ * Add a pingback url auto-discovery header for single posts of any post type.
+ */
+function onepress_pingback_header() {
+	if ( is_singular() && pings_open() ) {
+		echo '<link rel="pingback" href="' . esc_url( get_bloginfo( 'pingback_url' ) ) . '">';
+	}
+}
+add_action( 'wp_head', 'onepress_pingback_header' );
+
+/**
  * Adds custom classes to the array of body classes.
  *
  * @param array $classes Classes for the body element.
