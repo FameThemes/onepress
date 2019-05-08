6f170871ebbb0cbc84e6e34d94d3bf4d5a36ae9c
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2019-05-08 14:35:36 +0200

    Add support for wp_body_open
    WP 5.2 introduced a new `wp_body_open()` function that is used to trigger a `wp_body_open` action. This action is intended to allow developers to inject code immediately following the opening `<body>` tag.

--- header.php
+++ header.php
@@ -19,6 +19,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php do_action( 'wp_body_open' ); ?>
 <?php do_action( 'onepress_before_site_start' ); ?>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'onepress' ); ?></a>
