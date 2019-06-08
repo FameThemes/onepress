5658e77d35f506062a294e10bc84ab63dce1d0a1
Hernán Hegykozi <hernanjh@gmail.com>
2019-06-08 01:32:00 -0300

    hide title
    hide title in content-single.php simil content-page.php

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -10,7 +10,7 @@
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php //the_title( '<h1 class="entry-title">', '</h1>' ); ?>
         <?php if ( get_theme_mod( 'single_meta', 1 ) ) { ?>
 		<div class="entry-meta">
 			<?php onepress_posted_on(); ?>
