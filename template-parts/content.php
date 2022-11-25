6482869eaa88b5c60429312f381a062fb65634fe
shrimp2t <shrimp2t@gmail.com>
2022-11-25 09:54:42 +0700

    Add hook for loop posts

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying posts.
  *
@@ -8,48 +9,67 @@
  */
 
 $show_thumbnail = true;
-if ( get_theme_mod( 'onepress_hide_thumnail_if_not_exists', false ) ) {
-	if ( ! has_post_thumbnail() ) {
+if (get_theme_mod('onepress_hide_thumnail_if_not_exists', false)) {
+	if (!has_post_thumbnail()) {
 		$show_thumbnail = false;
 	}
 }
 
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class( array( 'list-article', 'clearfix' ) ); ?>>
-	<?php if ( $show_thumbnail ) { ?>
-	<div class="list-article-thumb">
-		<a href="<?php echo esc_url( get_permalink() ); ?>">
-			<?php
-			if ( has_post_thumbnail() ) {
-				the_post_thumbnail( 'onepress-blog-small' );
-			} else {
-				echo '<img alt="" src="' . get_template_directory_uri() . '/assets/images/placholder2.png' . '">';
-			}
-			?>
-		</a>
-	</div>
+<article id="post-<?php the_ID(); ?>" <?php post_class(array('list-article', 'clearfix')); ?>>
+	<?php if ($show_thumbnail) { ?>
+		<div class="list-article-thumb">
+			<a href="<?php echo esc_url(get_permalink()); ?>">
+				<?php
+				if (has_post_thumbnail()) {
+					the_post_thumbnail('onepress-blog-small');
+				} else {
+					echo '<img alt="" src="' . get_template_directory_uri() . '/assets/images/placholder2.png' . '">';
+				}
+				?>
+			</a>
+		</div>
 	<?php } ?>
 
 	<div class="list-article-content">
+		<?php
+		/**
+		 * Hook before article content
+		 *
+		 * @since 2.3.4
+		 */
+		do_action('onepress_loop_content_before');
+
+		?>
 		<div class="list-article-meta">
-			<?php the_category( ' / ' ); ?>
+			<?php the_category(' / '); ?>
 		</div>
 		<header class="entry-header">
-			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+			<?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
 		</header>
 		<div class="entry-excerpt">
 			<?php
-				the_excerpt();
+			the_excerpt();
 			?>
 			<?php
-				wp_link_pages(
-					array(
-						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'onepress' ),
-						'after'  => '</div>',
-					)
-				);
-				?>
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__('Pages:', 'onepress'),
+					'after'  => '</div>',
+				)
+			);
+			?>
 		</div>
+		<?php
+
+		/**
+		 * Hook after article content
+		 *
+		 * @since 2.3.4
+		 */
+		do_action('onepress_loop_content_after');
+
+		?>
 	</div>
 
 </article>
