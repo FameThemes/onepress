99f2032a2562a190d619d88922a193a5d7dac891
TruongSa <shrimp2t@gmail.com>
2026-04-13 20:16:05 +0700

    Tweak customizer styles: spacing and font weight
    Reduce top margin for .onepress-styling .states from 10px to 8px and set font-weight: 600 for the state/tab controls to improve visual weight. Changes applied to source SCSS and compiled CSS/RTL outputs, and the customizer asset version hash was bumped in customizer.asset.php.

--- assets/admin/customizer.asset.php
+++ assets/admin/customizer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd85e3dd7150c8d3363fb');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b5d80d3a10c181297a71');
