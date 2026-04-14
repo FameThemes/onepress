6a3d5405bc2c5ce2a49e1957366943d5ad337772
TruongSa <shrimp2t@gmail.com>
2026-04-14 11:38:29 +0700

    Use CSS variable for customizer panel padding
    Replace hardcoded padding on body.wp-customizer .media-modal-backdrop with the --opc-panel-spacing variable across SCSS, compiled CSS and RTL CSS. Also update assets/admin/customizer.asset.php version hash to reflect the rebuild. This makes panel spacing consistent with the theme variable.

--- assets/admin/customizer.asset.php
+++ assets/admin/customizer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '988704b75d0b13d9c05a');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e44d961b1601764aec9b');
